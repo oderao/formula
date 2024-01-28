7c94749dec8a66d2a13ca8ec070a134abd57d462
oderao <tripleo4u@gmail.com>
2024-01-28 17:17:21 +0100

    add custom field add util functions for sales invoice(server and cliient)

--- formula/public/js/sales_invoice.js
+++ formula/public/js/sales_invoice.js
@@ -1,6 +1,5 @@
 frappe.ui.form.on('Sales Invoice', {
     refresh(frm) {
-        console.log("hello")
         frm.set_query('uom', 'items', function(doc, cdt, cdn) {
             var d = locals[cdt][cdn];
             return {
@@ -10,5 +9,89 @@ frappe.ui.form.on('Sales Invoice', {
                 query:"formula.utils.get_item_uom",
             };
         });
+    },
+    custom_item_to_convert(frm){
+        //get uom list 
+        frm.set_value("custom_qty_to_convert", 0)
+
+        frm.set_value("custom_converted_rate", 0)
+        frm.set_value("custom_converted_qty", 0)
+
+        frappe.call({
+            method : "formula.utils.get_uom_list",
+            args : {
+                "item" : frm.doc.custom_item_to_convert
+            },
+            callback : function(r){
+                console.log(r)
+                if (r && r.message){
+					cur_frm.set_df_property('custom_convert_from_uom',"options",r.message)
+					cur_frm.set_df_property('custom_convert_to_uom',"options",r.message)
+                    frm.refresh_fields()
+
+                }
+            }
+        })
+    },
+    custom_qty_to_convert(frm){
+        if(frm.doc.custom_convert_from_uom && frm.doc.custom_convert_to_uom){
+            frappe.call({
+                method:"formula.utils.get_converted_rate_and_qty",
+                args:{
+                    "item": frm.doc.custom_item_to_convert,
+                    "qty" : frm.doc.custom_qty_to_convert,
+                    "convert_from" : frm.doc.custom_convert_from_uom,
+                    "convert_to" : frm.doc.custom_convert_to_uom
+                },
+                callback:function(r){
+                    frm.set_value("custom_converted_rate", r.message.converted_rate)
+                    frm.set_value("custom_converted_qty",r.message.converted_qty)
+                    frm.refresh_fields()
+                }
+            })
+        }
+        
+    },
+    validate(frm){
+        console.log("Hello")
+        frm.set_value("custom_converted_rate", "")
+        frm.set_value("custom_converted_qty","")
+        frm.set_value("custom_item_to_convert", "")
+        frm.set_value("custom_qty_to_convert",0)
     }
 })
+
+
+frappe.ui.form.on('Sales Invoice Item', {
+    // cdt is Child DocType name i.e Quotation Item
+    // cdn is the row name for e.g bbfcb8da6a
+    uom(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        // get item warehouse and rate by unit of measure
+        frappe.call({
+            method:"formula.utils.get_warehouse_and_rate",
+            args:{
+                "uom":row.uom,
+                "item":row.item_name
+            },
+            callback:function(r){
+                console.log(r)
+                row.warehouse = r.message.warehouse
+                row.rate = r.message.uom_rate
+                row.base_rate = r.message.uom_rate
+                row.price_list_rate = r.message.uom_rate
+                row.base_price_list_rate = r.message.uom_rate
+                row.qty = r.message.qty
+                // row.amount = r.message.uom_rate * row.qty
+                // row.base_amount = r.message.uom_rate * row.qty
+                cur_frm.refresh_field("items")
+                cur_frm.refresh_fields()
+                cur_frm.cscript.calculate_taxes_and_totals();
+
+
+            }
+
+        })
+    }
+})
+
--- formula/utils.py
+++ formula/utils.py
@@ -1,4 +1,5 @@
 import frappe
+from erpnext.stock.utils import get_stock_balance
 
 
 @frappe.whitelist()
@@ -15,3 +16,48 @@ def get_item_uom(doctype, txt, searchfield, start, page_len, filters):
 
 	return frappe.db.sql(query, filters)
 
+@frappe.whitelist()
+def get_warehouse_and_rate(item,uom):
+	if item and uom:
+		warehouse = frappe.db.get_value("UOM Conversion Detail",{"parent":item,"uom":uom},"custom_warehouse")
+		
+		uom_rate = frappe.db.get_value("UOM Conversion Detail",{"parent":item,"uom":uom},"custom_uom_rate")
+		qty = frappe.db.get_value("UOM Conversion Detail",{"parent":item,"uom":uom},"custom_minimum_qty")
+		
+		return {"uom_rate":uom_rate,"warehouse":warehouse,"qty":qty}
+
+@frappe.whitelist()
+def get_uom_list(item):
+    if item:
+        uom_list = frappe.get_list("UOM Conversion Detail",{"parent":item},["uom","custom_uom_rate","custom_warehouse"])
+        
+        return [i["uom"] + "/" + frappe.utils.fmt_money(i["custom_uom_rate"],currency="ZAR") + "/" + str(get_stock_balance(item,i["custom_warehouse"]))  for i in uom_list]
+    
+@frappe.whitelist()
+def get_converted_rate_and_qty(item,qty,convert_from,convert_to):
+    if item:
+        convert_from = convert_from.split("/")[0]
+        convert_to = convert_to.split("/")[0]
+        
+        convert_from = frappe.get_doc("UOM Conversion Detail",{"parent":item,"uom":convert_from})
+        convert_to = frappe.get_doc("UOM Conversion Detail",{"parent":item,"uom":convert_to})
+        
+        convert_from_rate = convert_from.custom_uom_rate
+        convert_to_rate = convert_to.custom_uom_rate
+        
+        convert_from_factor = convert_from.custom_conversion_factor #conversion factor
+        convert_to_factor= convert_to.custom_conversion_factor
+        
+        
+        conversion_rate =  convert_to_rate/convert_from_rate
+        
+        convert_to_minimum_qty = convert_to.custom_minimum_qty
+        
+        conversion_factor = convert_to_factor/convert_from_factor
+        
+        converted_qty = float(conversion_factor) * float(qty)
+        amount_to_sell_at = (float(converted_qty) * convert_to_rate)/convert_to_minimum_qty
+        
+        converted_rate = amount_to_sell_at
+        
+        return {"converted_rate":converted_rate,"converted_qty":converted_qty}
