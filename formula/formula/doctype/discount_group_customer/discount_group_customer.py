46bd53094dbbc241c648c5e023d138f78a972c4f
oderao <tripleo4u@gmail.com>
2024-02-12 09:16:59 +0100

    add discount gtoup customer table

---
+++ formula/formula/doctype/discount_group_customer/discount_group_customer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Nigmacorp and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DiscountGroupCustomer(Document):
+	pass
